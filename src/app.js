27d1eb1429cc31dfe146b7e8ddc8cbdaf4c9afd3
Hugo Silva <hugoflsilva@gmail.com>
2016-02-11 13:47:18 +0000

    Removed query string from the URL Webpack file minimizing flag added Dependency correction

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import React from 'react';
-    
 import { render } from 'react-dom'
-import { browserHistory, Router, Route } from 'react-router'
-
+import { Router, Route } from 'react-router'
 import MainApp from './components/MainPage/MainPage.jsx';
 import About from './components/AboutPage/AboutPage.jsx';
 import NoMatch from './components/notfound.jsx';
+import createHistory from 'history/lib/createHashHistory'
+
+let history = createHistory({
+    queryKey: false
+})
 
 render((
-    <Router history={browserHistory}>
+    <Router history={history}>
         <Route path="/" component={MainApp} />
         <Route path="/about" component={About} />
         <Route path="*" component={NoMatch}/>
     </Router>
 ), document.getElementById('app'))
-
-//ReactDOM.render(<MainApp data={Data.init()} />, document.getElementById('app'));
